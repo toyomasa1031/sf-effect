b48ee65730fbf380d15f2c7b3722f50a3a5c58de
toyomasa1031 <76829611+toyomasa1031@users.noreply.github.com>
2022-11-06 17:25:18 +0900

    ゆさぶりで、画面を消すようにしました。

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 15; index++) {
-        music.playTone(randint(0, 20) * 50 + 600, music.beat(BeatFraction.Eighth))
+    for (let index = 0; index < 5; index++) {
+        music.playTone(randint(10, 30) * 50 + 600, music.beat(BeatFraction.Quarter))
         led.toggle(randint(0, 4), randint(0, 4))
     }
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+})
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 15; index++) {
-        music.playTone(randint(0, 20) * 10 + 200, music.beat(BeatFraction.Eighth))
+    for (let index = 0; index < 5; index++) {
+        music.playTone(randint(0, 20) * 10 + 200, music.beat(BeatFraction.Quarter))
         led.toggle(randint(0, 4), randint(0, 4))
     }
 })
